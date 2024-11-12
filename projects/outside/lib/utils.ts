c044131dbcebc7a4a33fb153e3b4479d2d4f8fa2
Trevor Reed <trevor.reed@familysearch.org>
2024-11-11 23:03:30 -0700

    fix tty issue

--- projects/outside/lib/utils.ts
+++ projects/outside/lib/utils.ts
@@ -30,31 +30,13 @@ export class ConsoleManager {
   }
 
   clearLine() {
-    if (this.stdout.isTerminal()) {
-      const { columns } = Deno.consoleSize()
-      Deno.stdout.writeSync(encoder.encode(`\r`))
-      Deno.stdout.writeSync(encoder.encode(`${' '.repeat(columns)}`))
-      Deno.stdout.writeSync(encoder.encode(`\r`))
-    } else {
-      this.stdout.writeSync(encoder.encode('\x1b[2K'))
-    }
+    this.stdout.writeSync(encoder.encode('\x1b[2K'))
   }
 
   printNewline() {
     this.stdout.writeSync(encoder.encode('\n'))
   }
 }
-
-// export const moveToStartOfLine = () => Deno.stdout.writeSync(encoder.encode('\r'))
-// export const moveUpXLines = (x: number) => Deno.stdout.writeSync(encoder.encode(`\x1b[${x}A\r`))
-// export const moveDownXLines = (x: number) => Deno.stdout.writeSync(encoder.encode(`\x1b[${x}B\r`))
-// export const writeOver = (char: string, count: number) => {
-//   const { columns } = Deno.consoleSize()
-//   Deno.stdout.writeSync(encoder.encode(`\r`))
-//   Deno.stdout.writeSync(encoder.encode(`${' '.repeat(columns)}`))
-//   Deno.stdout.writeSync(encoder.encode(`\r`))
-// }
-// export const printNewline = () => console.log()
 
 export const getTimestamp = (): string => {
   const date = new Date()
